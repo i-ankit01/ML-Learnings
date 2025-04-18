8a6c7bc6efebc23f6d0c96511677189d16d2a9e9
i-ankit01 <ankit.common.acc@gmail.com>
2025-04-18 14:36:13 +0530

    improved model

--- test.py
+++ test.py
@@ -2,6 +2,10 @@ import pandas as pd
 import joblib
 model = joblib.load('price_prediction_model.pkl')
 
+df = pd.read_csv('CropPricePrediction/crop_price_dataset.csv')
+df_filtered = df[(df['month'] == '2025-03-01') & (df['commodity_name'] == 'Rice')]
+
+
 def predict_crop_price(year, month_num, crop_name):
     crops = [
         'Coconut', 'Coffee', 'Cotton', 'Ginger(Dry)', 'Groundnut', 'Jowar(Sorghum)',
@@ -23,4 +27,5 @@ def predict_crop_price(year, month_num, crop_name):
 
 price = predict_crop_price(year=2025, month_num=11, crop_name='Wheat')
 print(f"Predicted avg_modal_price: ₹{price}")
+print(f"Original avg_modal_price: ₹{df_filtered['avg_modal_price'].values[0]}")
 
